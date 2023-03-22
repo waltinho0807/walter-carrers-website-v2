08b1ac065c7cf9ce0fde9e0dec842d32f78e17e0
walter calegari <waltinhocalegari@hotmail.com>
2023-03-22 11:11:53 +0000

    Dynamic data, include API route

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ My site carrers
 
 # Links
 https://excalidraw.com/
+
 https://unsplash.com/pt-br
+
 https://www.lipsum.com/
+
 https://www.htmldog.com/
+
+https://getbootstrap.com/docs/5.0/getting-started/introduction/
--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data scientist',
+    'location': 'Bengalaru, India',
+    'salary': 'R$ 10,000, 00'
+  },
+  {
+    'id': 2,
+    'title': 'Data engener',
+    'location': 'New York, EUA',
+    'salary': 'R$ 15,000, 00'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend developer',
+    'location': 'Sao Paulo, Brasi',
+    'salary': 'R$ 8,000, 00'
+  }
+]
+
 @app.route("/")
 def hello_jovian():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  
+  
 print(__name__)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
