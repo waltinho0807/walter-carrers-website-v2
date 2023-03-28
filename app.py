b3e2aff485f550a17c61b2f3aa0a72193456ea9f
walter calegari <waltinhocalegari@hotmail.com>
2023-03-28 13:23:04 +0000

    Add templates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -14,7 +14,13 @@ def hello_jovian():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
-  
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not found", 404
+  return render_template('jobpage.html', job=job)
   
 print(__name__)
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -21,3 +21,17 @@ def load_jobs_from_db():
       jobs.append(row)
     return jobs
 
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),
+      {"val":id}
+    )
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else :
+      return dict(rows[0])
+      
+    
