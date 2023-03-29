db3a4e667940ea43a9f5c860cbf8ed130df4ac9d
walter calegari <waltinhocalegari@hotmail.com>
2023-03-29 15:09:15 +0000

    Add application in database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify,request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -21,6 +21,15 @@ def show_job(id):
   if not job:
     return "Not found", 404
   return render_template('jobpage.html', job=job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('aplication_submtted.html',
+                                    application = data,
+                                    job = job   ) 
   
 print(__name__)
 if __name__ == "__main__":
